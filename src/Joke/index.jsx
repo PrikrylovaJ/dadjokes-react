7a47a6e6e51788c454ba189d1da4e945424c3172
PrikrylovaJ <jprikrylova@email.cz>
2021-05-29 13:46:05 +0200

    Komponenta Joke

---
+++ src/Joke/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './style.css';
+
+const Joke = ({userAvatar, userName, text, likes, dislikes}) => {
+  // const [up, setUp] = useState(0);
+  // const [down,setDown] = useState(0);
+
+
+  // const handleUpClick = () => {
+  //   setUp(up + 1);
+  // }
+
+  // const handleDownClick = () => {
+  //   setDown(down + 1);
+  // }
+
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src={userAvatar} />
+            <p className="user-name">{userName}</p>
+          </div>
+
+          <p className="joke__text">
+            {text}
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <span id="likes-up" className="likes-count likes-count--up">{likes}</span>
+          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <span id="likes-down" className="likes-count likes-count--down">{dislikes}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Joke;
